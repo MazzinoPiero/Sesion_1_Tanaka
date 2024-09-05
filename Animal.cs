8a349d500ca58a1e0dd8a398a1bbea28026175b9
Piero Mazzino <132618050+MazzinoPiero@users.noreply.github.com>
2024-09-05 17:26:24 -0500

    Update Animal.cs
    Constructor animal

---
+++ Animal.cs
@@ -0,0 +1,16 @@
+using System;
+
+    class Animal
+    {
+        public string Name { get; set; }
+
+        public Animal(string name)
+        {
+            Name = name;
+        }
+
+        public virtual void MakeSound(string sound)
+        {
+            Console.WriteLine($"{Name} dice: {sound}");
+        }
+    }
